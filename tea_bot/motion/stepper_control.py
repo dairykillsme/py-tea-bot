1a2fde44df2964db66d873071b7bd02ae7b184d1
dairykillsme <dairykillsme>
2021-12-15 20:06:55 -0500

    Wrote control loop

--- tea_bot/motion/stepper_control.py
+++ tea_bot/motion/stepper_control.py
@@ -1,14 +1,53 @@
 from adafruit_motorkit import MotorKit
 import time
+import numpy as np
 
-kit = MotorKit()
+class MotionController:
+    THETA_1_RADS_STEP = np.pi / 100
+    THETA_2_RADS_STEP = np.pi / 200
+    
+    def __init__(self) -> None:
+        self.kit = MotorKit()
+        self.theta1_target = 0
+        self.theta2_target = 0
+    
+    def set_theta1(self, theta1):
+        self.theta1_target = theta1
+    
+    def set_theta2(self, theta2):
+        self.theta2_target = theta2
+    
+    def tick(self, theta1_meas, theta2_meas):
+        theta1_err = self.theta1_target - theta1_meas
+        if (np.abs(theta1_err) > MotionController.THETA_1_RADS_STEP):
+            if theta1_err > 0:
+                self.kit.stepper2.onestep(direction=2, style=1)
+            else:
+                self.kit.stepper2.onestep(direction=1, style=1)
+        
+        theta2_err = self.theta2_target - theta2_meas
+        if (np.abs(theta2_err) > MotionController.THETA_2_RADS_STEP):
+            if theta2_err > 0:
+                self.kit.stepper1.onestep(direction=2, style=1)
+            else:
+                self.kit.stepper1.onestep(direction=1, style=1)
+        
+        if (np.abs(theta1_err) > MotionController.THETA_1_RADS_STEP) or (np.abs(theta2_err) > MotionController.THETA_2_RADS_STEP):
+            return False
+        else:
+            return True
+            
 
-while True:
-    for i in range(100):
-        kit.stepper1.onestep(direction=1, style=2)
-        kit.stepper2.onestep(direction=2, style=2) # DUAL COIL is Higher Torque
-        time.sleep(0.1)
-    for i in range(100):
-        kit.stepper1.onestep(direction=2, style=2)
-        kit.stepper2.onestep(direction=1, style=2) # DUAL COIL is Higher Torque
+if __name__ == "__main__":
+    controller = MotionController()
+
+    theta_1_meas = 0
+    theta_2_meas = 0
+
+    controller.set_theta1(np.pi)
+    controller.set_theta2(-np.pi)
+
+    while not controller.tick(theta_1_meas, theta_2_meas):
+        theta_1_meas = np.min([theta_1_meas + controller.THETA_1_RADS_STEP, np.pi])
+        theta_2_meas = theta_2_meas - controller.THETA_2_RADS_STEP
         time.sleep(0.1)
