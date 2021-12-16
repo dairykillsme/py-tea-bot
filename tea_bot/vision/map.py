bbf434ed30d24d2630b83b9f0cf804c059c87a5b
George Philbrick <gphilbri@gmail.com>
2021-12-16 11:18:37 -0500

    Add obstacle detection

--- tea_bot/vision/map.py
+++ tea_bot/vision/map.py
@@ -10,7 +10,7 @@ from numpy.linalg import svd
 from .calibration import CameraCalibration
 
 CALIBRATION_FILE_DEFAULT = "share/calibration/nexigo_webcam/cfg.xml"
-MARKER_LENGTH = 40 # cm
+MARKER_LENGTH = 60 # mm
 MARKER_SEPARATION = 8 # cm 
 
 class WorldMap:
@@ -25,7 +25,7 @@ class WorldMap:
     ARM_END_EFFECTOR_ARUCO_ID = 3
 
     # Aruco board
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_1000)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_1000)
     board = aruco.GridBoard_create(5, 7, MARKER_LENGTH, MARKER_SEPARATION, aruco_dict)
     aruco_params = aruco.DetectorParameters_create()
 
@@ -66,15 +66,20 @@ class WorldMap:
     def stop(self):
         self.running = False
 
-    def get_arm_coords(self):
-        pass
-
     def get_obstacle_list(self):
         pass
 
     def process_markers(self, corners, ids):
         """ Assuming all corners are fixed at known locations, arm base is 0,0,
         and all markers are coplanar (or close enough for the purposes of this)
+        
+        Args:
+            corners - an ordered list of corners for each detected aruco marker.
+            ids - an ordered list of ids for each detected aruco marker
+        
+        Returns:
+            TRUE - all 7 markers detected
+            FALSE - not enough markers found in frame
         """
         if ids is not None:
             if len(ids) == 7:
@@ -94,15 +99,6 @@ class WorldMap:
                         self.arm_joint = np.mean(corners[id_idx], axis=1)
                     elif ids[id_idx] == WorldMap.ARM_END_EFFECTOR_ARUCO_ID:
                         self.arm_end_effector = np.mean(corners[id_idx], axis=1)
-                
-                # Make arm_base the origin
-                self.top_left -= self.arm_base
-                self.top_right -= self.arm_base
-                self.bottom_left -= self.arm_base
-                self.bottom_right -= self.arm_base
-                self.arm_joint -= self.arm_base
-                self.arm_end_effector -= self.arm_base
-                self.arm_base -= self.arm_base
 
                 # find affine transform knowing the location of the source points
                 real_points = np.float32([self.top_left[0], self.top_right[0], self.bottom_left[0], self.bottom_right[0]])
@@ -118,10 +114,46 @@ class WorldMap:
 
                 if not self.ready:
                     self.ready = True
+                
+                return True
+        return False
+    
+    def process_obstacles(self, grayscale_img):
+        # Converting image to a binary image 
+        # (black and white only image).
+        _,threshold = cv.threshold(grayscale_img, 110, 255, 
+                                    cv.THRESH_BINARY)
+        # Detecting shapes in image by selecting region 
+        # with same colors or intensity.
+        contours,_=cv.findContours(threshold, cv.RETR_TREE,
+                                    cv.CHAIN_APPROX_SIMPLE)
+        img_obstacles = []
+        mapped_obstacles = []
+        for cnt in contours:
+            area = cv.contourArea(cnt)
+
+            # We only care about large areas
+            if area > 700 and area < 10000:
+                approx = cv.approxPolyDP(cnt, 0.009*cv.arcLength(cnt, True), True)
+                perspective_mapped_polygon = self.persepective_transform_polygon(approx)
+                img_obstacles.append(approx)
+                mapped_obstacles.append(perspective_mapped_polygon)
+
+        self._img_obstacle_list = img_obstacles
+        self.obstacle_list = mapped_obstacles
+    
+    def find_goal(self, grayscale_img):
+        return
         
     def perspective_transform_point(self, point):
         q = np.dot(self.warp_mat, np.concatenate((point, [[1]]), axis=1).T)
         return np.divide(q[:2], q[2]).T
+
+    def persepective_transform_polygon(self, polygon):
+        new_poly = []
+        for point in polygon:
+            new_poly.append(self.perspective_transform_point(point))
+        return new_poly
 
     def plot_tagged_points(self):
         # Plot points
@@ -142,12 +174,20 @@ class WorldMap:
             plt.cla()
             plt.plot(ax[0], ay[0])
             plt.plot(bx[0], by[0])
+
+            for polygon in self.obstacle_list:
+                polygon = np.array(polygon)
+                px,py = polygon.T
+                plt.plot(px[0], py[0])
+
             plt.axis('scaled')
+            plt.xlim([-25, 25])
+            plt.ylim([-25, 25])
             plt.pause(0.0001)
 
 
     def process_image(self):
-        while (self.running):
+        while self.running:
             # Capture video and undistort it
             ret,frame = self.vid.read()
             frame_undistorted = self.calibration.undistort(frame)
@@ -158,14 +198,17 @@ class WorldMap:
             # Detect aruco markers
             corners,ids,rejected = aruco.detectMarkers(frame_undistorted_gray, self.aruco_dict, parameters=self.aruco_params)
             aruco.refineDetectedMarkers(frame_undistorted_gray, self.board, corners, ids, rejected)
-            self.process_markers(corners, ids)
+            markers_found = self.process_markers(corners, ids)
+            if markers_found:
+                self.process_obstacles(frame_undistorted_gray)
+                self.find_goal(frame_undistorted_gray)
 
-            if (self.show_feed):
+            if self.show_feed:
                 im_with_aruco_board = frame_undistorted
                 # if we found markers draw them and see whats up with the pose board
-                if ids is not None:
+                if markers_found:
                     im_with_aruco_board = aruco.drawDetectedMarkers(frame_undistorted, corners, ids, (0,255,0))
-
+                    cv.drawContours(im_with_aruco_board, self._img_obstacle_list, -1, (0, 0, 2555), 1)
                 cv.imshow("world_mapping", im_with_aruco_board)
                 # allow quitting
                 if cv.waitKey(1) & 0XFF == ord('q'):
